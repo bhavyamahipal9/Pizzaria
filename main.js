85fc8a81d6609e0af119d12139b6811231262192
bhavyamahipal9 <71548514+bhavyamahipal9@users.noreply.github.com>
2021-06-16 14:28:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+menu_list_array = ["Veg Margherita Pizza", 
+                   "Double Cheese Pizza",
+                   "Goldern Corn Pizza",
+                   "cheese mozzarella Pizza",
+                   "Sweet Corn Pizza"
+                    ];
+
+function getmenu(){
+
+var htmldata;
+
+htmldata = "<ol class='menulist'";
+
+menu_list_array.sort();
+
+for(var i = 0; i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+}
+
+htmldata=htmldata+"</ol>"
+
+document.getElementById("display_menu").innerHTML = htmldata;
+
+}
+
+function add_item(){
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i = 0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<div class="card"' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>';
+    }
+    htmldata=htmldata+"</section>";
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
+
+function add_top(){
+    var pizza_1 = document.getElementById("add_item").value;
+    menu_list_array.push(pizza_1);
+    document.getElementById("display_addedmenu").innerHTML = menu_list_array;
+    var htmldata;
+var item = document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i = 0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">' + '<img src="images/pizzaImg.png">' + menu_list_array[i] + '</div>';
+}
+htmldata=htmldata+"</section>";
+document.getElementById("display_addedmenu").innerHTML = htmldata;    
+}
